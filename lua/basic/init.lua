64a77733bbe69294c797936a272a7d568cf343b8
cameronpyne-smith <159719925+cameronpyne-smith@users.noreply.github.com>
2024-12-12 14:35:57 +0000

    Extract kickstart options and apply

--- lua/basic/init.lua
+++ lua/basic/init.lua
@@ -1,1 +1,6 @@
-require("basic.remap")
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+require 'basic.options'
+require 'basic.remap'
