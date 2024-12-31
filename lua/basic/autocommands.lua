60946486e081c74d43006db5aee5f1a58cc4630c
cameronpyne-smith <159719925+cameronpyne-smith@users.noreply.github.com>
2024-12-31 18:57:56 +0000

    Set comment string autocommands - disabled

--- lua/basic/autocommands.lua
+++ lua/basic/autocommands.lua
@@ -4,10 +4,24 @@
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
+
+-- Commenting
+-- vim.api.nvim_create_autocmd("FileType", {
+--     pattern = "python",
+--     command = "setlocal commentstring=#\\ %s",
+-- })
+-- vim.api.nvim_create_autocmd("FileType", {
+--     pattern = "c,cpp",
+--     command = "setlocal commentstring=//\\ %s",
+-- })
+-- vim.api.nvim_create_autocmd("FileType", {
+--     pattern = "html",
+--     command = "setlocal commentstring=<!--\\ %s\\ -->",
+-- })
