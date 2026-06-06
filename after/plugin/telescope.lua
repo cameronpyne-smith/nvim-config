a94f8ab5c9a7f29d5b7baee92870b5ab763b0a6d
cameronpyne-smith <c.pyne.smith@gmail.com>
2026-06-06 01:06:10 +0100

    Fix ff to show all files including hidden and gitignored

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,7 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find all files' })
+vim.keymap.set('n', '<leader>ff', function()
+	builtin.find_files({ follow = true, no_ignore = true, hidden = true })
+end, { desc = 'Telescope find all files' })
 vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = 'Telescope find git files' })
 vim.keymap.set('n', '<leader>fl', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fs', function()
